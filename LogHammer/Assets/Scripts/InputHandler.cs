d6cb88a4186d66550088afa65e63ff864644ae1a
Hema <hema.dubal95@gmail.com>
2017-05-29 20:09:34 +0530

    Hema: Started with the basics of the touch inputs

---
+++ LogHammer/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This class will handle all the touch inputs via events. It is a singleton class. Register to events by subscribing to them.
+public class InputHandler : MonoBehaviour
+{
+    //declaring a variable instance
+    private static InputHandler instance;
+    public delegate void Inputs();
+
+    public event Inputs Tap;
+    public event Inputs DoubleTaps;
+    public event Inputs ZoomIn;
+    public event Inputs ZoomOut;
+    public event Inputs Drag;
+    public event Inputs Swipe;
+    public event Inputs Hold;
+
+    private float m_doubleTapThreshold = 10f;
+
+    private TapInput m_tapInput;                                   //get the coordinated where the tap took place
+
+    //constructor
+    private InputHandler()
+    { }
+
+    //defining the Instance property of the instance variable
+    public static InputHandler Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new InputHandler();
+            }
+            return instance;
+        }
+    }
+
+    public void OnTap()
+    {
+        //call the tap event. All the methods susbscribed to Tap will be called.
+        if (Tap != null)
+            Tap();
+    }
+
+
+    public void OnDoubleTap()
+    {
+        if (DoubleTaps != null)
+            DoubleTaps();
+    }
+
+    public void OnZoomIn()
+    {
+        if (ZoomIn != null)
+            ZoomIn();
+    }
+
+    public void OnZoomOut()
+    {
+        if (ZoomOut != null)
+            ZoomOut();
+    }
+
+    private void Update()
+    {
+        //detect tap
+        if (Input.touchCount == 1)
+        {
+            Debug.Log("Tap event is detected");
+            OnTap();
+        }
+
+        //detect doubletap, zoom in and zoom out events since all these 
+        if (Input.touchCount == 2)
+        {
+            Touch touchzero = Input.GetTouch(0);
+            Touch touchone = Input.GetTouch(1);
+
+            //detect doubletap
+            if (Mathf.Abs(Vector3.Distance(touchzero.position, touchone.position)) < m_doubleTapThreshold)
+            {
+                Debug.Log("Double tap is detected");
+                OnDoubleTap();
+            }
+
+            //detect zoom in and zoom out
+            Vector2 touchzeroInPreviousFrame = touchzero.position - touchzero.deltaPosition;
+            Vector2 touchoneInPreviousFrame = touchone.position - touchone.deltaPosition;
+
+            float differenceInPreviousFrame = (touchoneInPreviousFrame - touchzeroInPreviousFrame).magnitude;
+            float differenceInThisFrame = (touchone.position - touchzero.position).magnitude;
+
+            if(differenceInThisFrame > differenceInPreviousFrame)
+            {
+                OnZoomIn();
+            }
+            else if(differenceInThisFrame < differenceInPreviousFrame)
+            {
+                OnZoomOut();
+            }
+        }
+
+        
+    }
+}
---
+++ LogHammer/Assets/Scripts/Temp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour
+{
+    void OnEnable()
+    {
+        //Subscribe to the Tap event
+        InputHandler.Instance.Tap += Instance_Tap;
+    }
+
+    void OnDisable()
+    {
+        //Unsubscribe to the Tap Event
+        InputHandler.Instance.Tap -= Instance_Tap;
+    }
+
+    //Function that got registered
+    private void Instance_Tap()
+    {
+        print("Instance_Tap is called");
+    }
+
+}
