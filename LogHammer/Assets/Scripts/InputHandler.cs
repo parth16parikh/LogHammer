4761c8d7801838d4cd59173028adb247a868377f
Hema <hema.dubal95@gmail.com>
2017-06-04 14:07:51 +0530

    Hema: tap and double tap events detected successfully

--- LogHammer/Assets/Scripts/InputHandler.cs
+++ LogHammer/Assets/Scripts/InputHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //This class will handle all the touch inputs via events. It is a singleton class. Register to events by subscribing to them.
 public class InputHandler : MonoBehaviour
@@ -17,9 +18,15 @@ public class InputHandler : MonoBehaviour
     public event Inputs Swipe;
     public event Inputs Hold;
 
-    private float m_doubleTapThreshold = 10f;
+    public Text text;
 
-    private TapInput m_tapInput;                                   //get the coordinated where the tap took place
+    private float m_doubleTapTimeThreshold = 0.5f;
+    private float m_doubleTapDistanceThreshold = 60f;
+
+    private TLTouch m_currentTouch;
+    private TLTouch m_previousTouch;
+
+    private float m_tapThreshold = 0.5f;
 
     //constructor
     private InputHandler()
@@ -64,45 +71,76 @@ public class InputHandler : MonoBehaviour
             ZoomOut();
     }
 
+    private void Start()
+    {
+        m_currentTouch = new TLTouch();
+        m_previousTouch = new TLTouch();
+    }
+
     private void Update()
     {
         //detect tap
         if (Input.touchCount == 1)
         {
-            Debug.Log("Tap event is detected");
-            OnTap();
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                m_currentTouch.m_touchStartPosition = touch.position;
+                m_currentTouch.m_startTime = Time.time;
+                Debug.Log("Start Time " + m_currentTouch.m_startTime);
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                m_currentTouch.m_touchEndPosition = touch.position;
+                m_currentTouch.m_endTime = Time.time;
+                Debug.Log("End Time " + m_currentTouch.m_endTime);
+                m_currentTouch.m_holdTime = m_currentTouch.m_endTime - m_currentTouch.m_startTime;
+                Debug.Log("Hold Time " + m_currentTouch.m_holdTime);
+                if (m_currentTouch.m_holdTime < m_tapThreshold)
+                {
+                    text.text = "Tap event";
+                    OnTap();
+                }
+
+                float diffBtwnTaps = m_currentTouch.m_startTime - m_previousTouch.m_endTime;
+                float diffInDistanceBtwnTaps = Vector2.Distance(m_currentTouch.m_touchStartPosition, m_previousTouch.m_touchStartPosition);
+
+                Debug.Log("Difference 1 " + diffBtwnTaps + " Difference 2 : " + diffInDistanceBtwnTaps);
+
+                if (diffBtwnTaps <= m_doubleTapTimeThreshold && diffInDistanceBtwnTaps <= m_doubleTapDistanceThreshold)
+                {
+                    text.text = "Double Tap";
+                    OnDoubleTap();
+                }
+                m_previousTouch = new TLTouch(m_currentTouch);
+            }
+
+
         }
 
-        //detect doubletap, zoom in and zoom out events since all these 
-        if (Input.touchCount == 2)
-        {
-            Touch touchzero = Input.GetTouch(0);
-            Touch touchone = Input.GetTouch(1);
+        ////detect doubletap, zoom in and zoom out events since all these 
+        //if (Input.touchCount >= 2)
+        //{
+        //    Touch touchzero = Input.GetTouch(0);
+        //    Touch touchone = Input.GetTouch(1);
 
-            //detect doubletap
-            if (Mathf.Abs(Vector3.Distance(touchzero.position, touchone.position)) < m_doubleTapThreshold)
-            {
-                Debug.Log("Double tap is detected");
-                OnDoubleTap();
-            }
+        //    //detect zoom in and zoom out
+        //    Vector2 touchzeroInPreviousFrame = touchzero.position - touchzero.deltaPosition;
+        //    Vector2 touchoneInPreviousFrame = touchone.position - touchone.deltaPosition;
 
-            //detect zoom in and zoom out
-            Vector2 touchzeroInPreviousFrame = touchzero.position - touchzero.deltaPosition;
-            Vector2 touchoneInPreviousFrame = touchone.position - touchone.deltaPosition;
+        //    float differenceInPreviousFrame = (touchoneInPreviousFrame - touchzeroInPreviousFrame).magnitude;
+        //    float differenceInThisFrame = (touchone.position - touchzero.position).magnitude;
 
-            float differenceInPreviousFrame = (touchoneInPreviousFrame - touchzeroInPreviousFrame).magnitude;
-            float differenceInThisFrame = (touchone.position - touchzero.position).magnitude;
+        //    if(differenceInThisFrame > differenceInPreviousFrame)
+        //    {
+        //        OnZoomIn();
+        //    }
+        //    else if(differenceInThisFrame < differenceInPreviousFrame)
+        //    {
+        //        OnZoomOut();
+        //    }
+        //}
 
-            if(differenceInThisFrame > differenceInPreviousFrame)
-            {
-                OnZoomIn();
-            }
-            else if(differenceInThisFrame < differenceInPreviousFrame)
-            {
-                OnZoomOut();
-            }
-        }
 
-        
     }
 }
---
+++ LogHammer/Assets/Scripts/TLTouch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TLTouch
+{
+    public TLTouch(TLTouch tlTouch)
+    {
+        m_touchStartPosition = tlTouch.m_touchStartPosition;
+        m_touchEndPosition = tlTouch.m_touchEndPosition;
+        m_startTime = tlTouch.m_startTime;
+        m_endTime = tlTouch.m_endTime;
+        m_holdTime = tlTouch.m_holdTime;
+    }
+
+    public TLTouch()
+    { }
+
+    //stores all the basic information about a particular touch
+    public Vector2 m_touchStartPosition, m_touchEndPosition;
+    public float m_startTime, m_endTime, m_holdTime;
+}
