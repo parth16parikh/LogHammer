f491052715635d7e298efff43582e107915236ad
Hema <hema.dubal95@gmail.com>
2017-06-10 02:25:44 +0530

    Hema: Detected the Swipe and Hold events and Re-organized the code

--- LogHammer/Assets/Scripts/InputHandler.cs
+++ LogHammer/Assets/Scripts/InputHandler.cs
@@ -6,27 +6,46 @@ using UnityEngine.UI;
 //This class will handle all the touch inputs via events. It is a singleton class. Register to events by subscribing to them.
 public class InputHandler : MonoBehaviour
 {
+    public enum SwipeDirection
+    {
+        NONE,
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
+
     //declaring a variable instance
     private static InputHandler instance;
     public delegate void Inputs();
 
+    //whether multi-touch is enabled
+    public bool EnableMultiTouch;
+
+    //declare all the public Events
     public event Inputs Tap;
     public event Inputs DoubleTaps;
-    public event Inputs ZoomIn;
-    public event Inputs ZoomOut;
     public event Inputs Drag;
     public event Inputs Swipe;
     public event Inputs Hold;
 
+    //temperory text for testing
     public Text text;
 
+    //all the public threshold variables
     private float m_doubleTapTimeThreshold = 0.5f;
-    private float m_doubleTapDistanceThreshold = 60f;
+    private float m_tapRadiusThreshold = 60f;
+    private float m_tapHoldTimeThreshold = 0.3f;
+    private float m_swipeWidth = 60f;
+    private float m_swipeHoldTimeThreshold = 0.5f;
+    private float m_swipeDistanceThreshold = 80f;
+    private float m_dragHoldThreshold;
+    private float m_holdThresholdMinimum = 0.7f;
+    private float m_holdThresholdMaximum = 2f;
 
+    //TLTouch variables to store information regarding current and previous touches
     private TLTouch m_currentTouch;
     private TLTouch m_previousTouch;
-
-    private float m_tapThreshold = 0.5f;
 
     //constructor
     private InputHandler()
@@ -45,77 +64,82 @@ public class InputHandler : MonoBehaviour
         }
     }
 
+    //call the tap event. All the methods susbscribed to Tap will be called.
     public void OnTap()
     {
-        //call the tap event. All the methods susbscribed to Tap will be called.
         if (Tap != null)
             Tap();
     }
 
-
+    //call the double tap event. All the methods susbscribed to DoubleTap will be called.
     public void OnDoubleTap()
     {
         if (DoubleTaps != null)
             DoubleTaps();
     }
 
-    public void OnZoomIn()
-    {
-        if (ZoomIn != null)
-            ZoomIn();
-    }
-
-    public void OnZoomOut()
-    {
-        if (ZoomOut != null)
-            ZoomOut();
-    }
-
-    private void Start()
+    public void Start()
     {
         m_currentTouch = new TLTouch();
         m_previousTouch = new TLTouch();
     }
 
+    //Called Every frame
     private void Update()
     {
-        //detect tap
         if (Input.touchCount == 1)
         {
             Touch touch = Input.GetTouch(0);
+
             if (touch.phase == TouchPhase.Began)
             {
-                m_currentTouch.m_touchStartPosition = touch.position;
-                m_currentTouch.m_startTime = Time.time;
-                Debug.Log("Start Time " + m_currentTouch.m_startTime);
+                m_currentTouch.SetTouchStartInfo(touch.position, Time.time);
             }
+
             if (touch.phase == TouchPhase.Ended)
             {
-                m_currentTouch.m_touchEndPosition = touch.position;
-                m_currentTouch.m_endTime = Time.time;
-                Debug.Log("End Time " + m_currentTouch.m_endTime);
-                m_currentTouch.m_holdTime = m_currentTouch.m_endTime - m_currentTouch.m_startTime;
-                Debug.Log("Hold Time " + m_currentTouch.m_holdTime);
-                if (m_currentTouch.m_holdTime < m_tapThreshold)
-                {
-                    text.text = "Tap event";
-                    OnTap();
-                }
+                m_currentTouch.SetTouchEndInfo(touch.position, Time.time);
 
-                float diffBtwnTaps = m_currentTouch.m_startTime - m_previousTouch.m_endTime;
-                float diffInDistanceBtwnTaps = Vector2.Distance(m_currentTouch.m_touchStartPosition, m_previousTouch.m_touchStartPosition);
-
-                Debug.Log("Difference 1 " + diffBtwnTaps + " Difference 2 : " + diffInDistanceBtwnTaps);
-
-                if (diffBtwnTaps <= m_doubleTapTimeThreshold && diffInDistanceBtwnTaps <= m_doubleTapDistanceThreshold)
+                float timeDifferenceBetweenTaps = m_currentTouch.EndTime - m_previousTouch.EndTime;
+                float distanceBetweenTaps = Vector2.Distance(m_currentTouch.EndPosition, m_previousTouch.EndPosition);
+                
+                if (timeDifferenceBetweenTaps <= m_doubleTapTimeThreshold && distanceBetweenTaps <= m_tapRadiusThreshold && m_currentTouch.HoldDistance <= m_tapRadiusThreshold && m_previousTouch.HoldDistance <= m_tapRadiusThreshold)
                 {
                     text.text = "Double Tap";
                     OnDoubleTap();
                 }
+                else if (m_currentTouch.HoldTime < m_tapHoldTimeThreshold && m_currentTouch.HoldDistance <= m_tapRadiusThreshold)
+                {
+                    text.text = "Tap event";
+                    OnTap();
+                }
+                if (m_currentTouch.HoldTime <= m_swipeHoldTimeThreshold && m_currentTouch.HoldDistance >= m_swipeDistanceThreshold)
+                {
+                    Vector2 swipeData = m_currentTouch.HoldVector;
+
+                    bool swipeIsVertical = Mathf.Abs(swipeData.x) < m_swipeWidth;
+                    bool swipeIsHorizontal = Mathf.Abs(swipeData.y) < m_swipeWidth;
+
+                    if (swipeData.y > 0f && swipeIsVertical)
+                        text.text = "UP";
+
+                    if (swipeData.y < 0f && swipeIsVertical)
+                        text.text = "DOWN";
+
+                    if (swipeData.x > 0f && swipeIsHorizontal)
+                        text.text = "RIGHT";
+
+                    if (swipeData.x < 0f && swipeIsHorizontal)
+                        text.text = "LEFT";
+                }
+
+                if (m_currentTouch.HoldTime >= m_holdThresholdMinimum && m_currentTouch.HoldTime <= m_holdThresholdMaximum && m_currentTouch.HoldDistance <= m_tapRadiusThreshold)
+                {
+                    text.text = "HOLD";
+                }
+                //if(m_currentTouch.m_holdTime >= m_dragHoldThreshold && Vector2.Distance(m_c))
                 m_previousTouch = new TLTouch(m_currentTouch);
             }
-
-
         }
 
         ////detect doubletap, zoom in and zoom out events since all these 
--- LogHammer/Assets/Scripts/TLTouch.cs
+++ LogHammer/Assets/Scripts/TLTouch.cs
@@ -4,19 +4,122 @@ using UnityEngine;
 
 public class TLTouch
 {
+    //copy constructor
     public TLTouch(TLTouch tlTouch)
     {
-        m_touchStartPosition = tlTouch.m_touchStartPosition;
-        m_touchEndPosition = tlTouch.m_touchEndPosition;
-        m_startTime = tlTouch.m_startTime;
-        m_endTime = tlTouch.m_endTime;
-        m_holdTime = tlTouch.m_holdTime;
+        startPosition = tlTouch.startPosition;
+        endPosition = tlTouch.endPosition;
+        startTime = tlTouch.startTime;
+        endTime = tlTouch.endTime;
+        holdTime = tlTouch.holdTime;
     }
 
     public TLTouch()
-    { }
+    {
+    }
 
     //stores all the basic information about a particular touch
-    public Vector2 m_touchStartPosition, m_touchEndPosition;
-    public float m_startTime, m_endTime, m_holdTime;
+    private Vector2 startPosition, endPosition;
+    private float startTime, endTime, holdTime, holdDistance;
+    private Vector2 holdVector;
+
+    //define the properties of above private variables
+    public Vector2 StartPosition
+    {
+        get
+        {
+            return startPosition;
+        }
+        set
+        {
+            startPosition = value;
+        }
+    }
+
+    public Vector2 EndPosition
+    {
+        get
+        {
+            return endPosition;
+        }
+        set
+        {
+            endPosition = value;
+        }
+    }
+
+    public float StartTime
+    {
+        get
+        {
+            return startTime;
+        }
+        set
+        {
+            startTime = value;
+        }
+    }
+
+    public float EndTime
+    {
+        get
+        {
+            return endTime;
+        }
+        set
+        {
+            endTime = value;
+        }
+    }
+    
+    public Vector2 HoldVector
+    {
+        set
+        {
+            holdVector = value;
+        }
+        get
+        {
+            return holdVector;
+        }
+    }
+
+    public float HoldTime
+    {
+        set
+        {
+            holdTime = value;
+        }
+        get
+        {
+            return holdTime;
+        }
+    }
+
+    public float HoldDistance
+    {
+        get
+        {
+            return holdDistance;
+        }
+        set
+        {
+            holdDistance = value;
+        }
+    }
+
+    public void SetTouchStartInfo(Vector2 startPos, float startTime)
+    {
+        StartPosition = startPos;
+        StartTime = startTime;
+    }
+
+    public void SetTouchEndInfo(Vector2 endPos, float endTime)
+    {
+        EndPosition = endPos;
+        EndTime = endTime;
+        HoldVector = new Vector2(EndPosition.x - StartPosition.x, EndPosition.y - StartPosition.y);
+        HoldDistance = HoldVector.magnitude;
+        HoldTime = EndTime - StartTime;
+    }
 }
